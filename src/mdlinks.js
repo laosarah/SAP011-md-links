dd45c3702b32ba91340e6328d9758ab817748cc1
laosarah <139395413+laosarah@users.noreply.github.com>
2023-10-28 17:04:34 -0300

    Função mdLinks

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,6 +1,30 @@
-// const fs = require('fs');
+const fs = require('fs');
+const { linkValidation } = require('./index.js');
 
 // função mdLinks
+function mdLinks(path, options) {
+  return fs.promises.readFile(path, 'utf8')
+  .then ((fileContent) => {
 
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const newArray = [...fileContent.matchAll(regex)];
 
-// module.exports = { mdLinks };
+    const links = newArray.map((link) => ({
+      text: link[1],
+      url: link[2],
+      file: path,
+    })
+    )
+
+    if (options.validate) {
+      const validations = links.map((link) => 
+        linkValidation(link)
+      );
+      return Promise.all(validations);
+    }
+    
+    return links;
+  })
+}
+
+module.exports = { mdLinks };
